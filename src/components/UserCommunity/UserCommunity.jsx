68eabb236c440c7bc947e6a85bcc3b6d7135a856
siddiquenaim <naim.niter.tex06@gmail.com>
2023-08-06 23:22:56 +0600

    Fetched user specific community data

--- src/components/UserCommunity/UserCommunity.jsx
+++ src/components/UserCommunity/UserCommunity.jsx
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider/AuthProvider";
 
 const UserCommunity = () => {
+  const { user } = useContext(AuthContext);
   const [userCommunity, setUserCommunity] = useState([]);
 
   const url = `http://localhost:5000/allCommunities?userEmail=${user?.email}`;
 
   useEffect(() => {
-    fetch(url);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setUserCommunity(data[0]));
   });
+
   return (
     <div>
-      <h1>You haven't created a community yet</h1>
-      <div className="my-4 text-center">
-        <Link to="create-community">
-          <button className="btn">Create Your Community</button>
-        </Link>
-      </div>
+      {userCommunity.length === 0 ? (
+        <>
+          <h1>You haven't created a community yet</h1>
+          <div className="my-4 text-center">
+            <Link to="create-community">
+              <button className="btn">Create Your Community</button>
+            </Link>
+          </div>
+        </>
+      ) : (
+        <>Community Name = {userCommunity?.name}</>
+      )}
     </div>
   );
 };
